107ebb2bc4708f6edd07108cb4afea15397d47fe
Костя Иванчик <kostya.ivanchik@mail.ru>
2022-05-31 12:41:19 +0300

    added coin table

---
+++ prisma/migrations/20220530144707_/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Coin" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "price" DECIMAL(65,30) NOT NULL DEFAULT 0,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" INTEGER,
+
+    CONSTRAINT "Coin_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Coin" ADD CONSTRAINT "Coin_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
---
+++ src/index.ts
@@ -0,0 +1,76 @@
+import { PrismaClient } from '@prisma/client'
+import express from 'express'
+
+const prisma = new PrismaClient()
+const app = express()
+
+app.use(express.json())
+
+app.get(`/users`, async (req, res) => {
+    const users = await prisma.user.findMany()
+    res.json(users)
+})
+app.get(`/feed`, async(req, res) => {
+    const coins = await prisma.coin.findMany({
+        where: {published:true},
+        include: {author: true}
+    })
+    res.json(coins)
+})
+
+app.get(`/coin/:id`, async (req, res) => {
+    const { id } = req.params
+    const coin = await prisma.coin.findFirst({
+        where: { id: Number(id) },
+    })
+    res.json(coin)
+})
+
+app.post(`/user`, async (req, res) => {
+    const resoult = await prisma.user.create({
+        data: { ...req.body },
+    })
+    res.json(resoult)
+})
+
+
+app.post(`/coin`, async (req, res) => {
+    const { title, content, authorEmail } = req.body
+    const result = await prisma.coin.create({
+        data: {
+            title,
+            price: 0,
+            published: false,
+            author: { connect: { email: authorEmail }}
+        },
+    })
+    res.json(result)
+})
+app.put(`/coin/publish/:id`, async (req, res) => {
+    const { id } = req.params
+    const coin = await prisma.coin.update({
+        where: { id: Number(id)},
+        data: {published: true},
+    })
+    res.json(coin)
+})
+
+app.delete(`/coin/:id`, async (req, res) => {
+    const { id } = req.params
+    const coin = await prisma.coin.delete({
+        where: {id: Number(id) },
+    })
+    res.json(coin)
+})
+
+app.delete(`/users/:id`, async (req, res) => {
+    const { id } = req.params
+    const users = await prisma.user.delete({
+        where: { id: Number(id) },
+    })
+    res.json(users)
+})
+
+app.listen(5433, () => 
+    console.log(`REST API server ready at: http://localhost:5433`),
+)
